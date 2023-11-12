e546c5d17544a6fbef59e9aeada683656ce14b2b
Alex Standiford <alex@standiford.us>
2023-11-12 16:42:43 -0600

    Screen Support

--- lib/Interfaces/EventStrategy.php
+++ lib/Interfaces/EventStrategy.php
@@ -5,7 +5,7 @@ interface EventStrategy
 {
     public function broadcast(Event $event): void;
 
-    public function attach(string $event, callable $action, ?int $priority): void;
+    public function attach(string $event, callable $action, ?int $priority = null): void;
 
-    public function detach(string $event, callable $action, ?int $priority): void;
+    public function detach(string $event, callable $action, ?int $priority = null): void;
 }
