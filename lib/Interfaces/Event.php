0a4532cb251eb2b5c303b6548d4d169d1eb2ebde
Alex Standiford <alex@standiford.us>
2024-01-31 10:48:16 -0500

    Component progress

--- lib/Interfaces/Event.php
+++ lib/Interfaces/Event.php
@@ -4,5 +4,10 @@ namespace PHPNomad\Events\Interfaces;
 
 interface Event
 {
+    /**
+     * Returns the unique identifier for this object.
+     *
+     * @return string The unique identifier for this object.
+     */
     public static function getId(): string;
 }
